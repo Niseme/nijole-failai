7cecc1482d5475316f3015a321ae0f79ce8be67f
Rimantas <belauzas@yahoo.com>
2021-10-27 13:00:19 +0300

    Async

--- index.js
+++ index.js
@@ -1,20 +1,62 @@
-const data = require('./lib/data.js');
+// const data = require('./lib/data.js');
+const asyncData = require('./lib/async-data.js');
 
-function parodykFailoTurini(err, content) {
-    if (err) {
-        console.log(err, content);
-        return false;
-    }
+// function parodykFailoTurini(err, content) {
+//     if (err) {
+//         console.log(err, content);
+//         return false;
+//     }
 
-    console.log(content);
-    console.log(content.name);
-    console.log(content.age);
-}
+//     console.log(content);
+//     console.log(content.name);
+//     console.log(content.age);
+// }
 
-data.read('students', 'asd', parodykFailoTurini);
-data.read('students', 'petras', parodykFailoTurini);
-data.read('students', 'maryte', parodykFailoTurini);
+// data.read('students', 'asd', parodykFailoTurini);
+// data.read('students', 'petras', parodykFailoTurini);
+// data.read('students', 'maryte', parodykFailoTurini);
 
-data.folderContent('students', (err, list) => {
-    console.log(list);
-});
+// data.folderContent('students', (err, list) => {
+//     console.log(list);
+// });
+
+// data.create('books', 'grybu-karas', { text: 'Simtas grybu susirinko' }, (err, msg) => {
+//     console.log(err, msg);
+
+//     data.read('books', 'grybu-karas', (err, content) => {
+//         console.log(err, content);
+
+//         content.img = 'grybas.png';
+
+//         data.update('books', 'grybu-karas', content, (err, msg) => {
+//             console.log(err, msg);
+
+//             data.read('books', 'grybu-karas', (err, content) => {
+//                 console.log(err, content);
+
+//                 data.delete('books', 'grybu-karas', (err, msg) => {
+//                     console.log(err, msg);
+//                 })
+//             })
+//         })
+//     })
+// })
+
+// IIFE
+(async () => {
+    const bc = await asyncData.create('books', 'zuikis-puikis', { text: 'Simtas zuikiu susirinko' });
+    console.log(bc);
+
+    const br1 = await asyncData.read('books', 'zuikis-puikis');
+    console.log(br1);
+
+    br1.text += ' net zalia girele linko';
+    const bu = await asyncData.update('books', 'zuikis-puikis', br1);
+    console.log(bu);
+
+    const br2 = await asyncData.read('books', 'zuikis-puikis');
+    console.log(br2);
+
+    const bd = await asyncData.delete('books', 'zuikis-puikis');
+    console.log(bd);
+})();
